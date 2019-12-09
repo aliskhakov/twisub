96f7e3a9e977b90d352dcabf342e6b2d65cc77d8
Albert Iskhakov <aliskhakov@gmail.com>
2019-12-10 00:04:25 +0300

    Add SSE notification endpoint.

--- ssenotifications/src/main/java/com/jstructure/twisub/ssenotifications/service/NotificationDataService.java
+++ ssenotifications/src/main/java/com/jstructure/twisub/ssenotifications/service/NotificationDataService.java
@@ -1,11 +1,14 @@
 package com.jstructure.twisub.ssenotifications.service;
 
 import com.jstructure.twisub.ssenotifications.dto.NotificationDto;
+import reactor.core.publisher.Flux;
 
 public interface NotificationDataService {
 
     void add(NotificationDto notification);
 
     NotificationDto get(String to);
+
+    Flux<NotificationDto> getFlux(String to);
 
 }
